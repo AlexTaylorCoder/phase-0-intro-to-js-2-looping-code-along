dedee12b6f1559fd150e8add5e44866cbe5e5a23
AlexTaylor <alextaylor515@gmail.com>
2022-04-27 16:46:14 -0400

    commit tree d380011b820cc5e3555f2ef18a3510368c1556f8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr,event) {
+    const nArr = []
+    for (let name of arr) {
+        nArr.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+    return nArr;
+}
+
+function countDown(num) {
+    if (parseInt(num) > 0) {
+        while (num >= 0) {
+            console.log(num)
+            num--;
+        }
+    } 
+}
